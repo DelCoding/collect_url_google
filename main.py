02bb5f8a6c7f4be7ecb2500c8c49c56f646ac705
Junay <jay200080@163.com>
2017-08-01 11:39:58 +0800

    尝试自动化搜索GOOGLE

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 #coding: UTF-8
 
-import selenium
+from selenium import webdriver
 import re
+
+browser = webdriver.Chrome()
+url = "https://www.google.com"
+browser.get(url)
+
+inputs = browser.find_element_by_name('q')
+inputs.send_keys('iphone8')
+button = browser.find_element_by_name('btnK')
+button.click()
